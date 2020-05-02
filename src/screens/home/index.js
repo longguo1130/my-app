4ce6c9a2b260cba9f1f164a758b4bdd3e8513de2
dongming <dongming0401@gmail.comgit config --global user.email dongming0401@gmail.com>
2020-05-02 11:02:50 -0400

    1

---
+++ src/screens/home/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./styles.css";
+
+function Home() {
+  return (
+    <div className="home-page">
+      <h1>CRÉ4TIVITÉ</h1>
+      <div className="home-description">
+        <h3>nous transformons vos idées en produits digitaux.</h3>
+        <h3> les entrepreneurs, agences et start-ups nous font</h3>
+        <h3> confiance pour nous confier leurs idées les plus </h3>
+        <h3> ambitieuses.</h3>
+   
+      </div>
+    </div>
+  );
+}
+
+export default Home;
